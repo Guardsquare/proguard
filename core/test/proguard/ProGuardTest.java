7bc1fdeb4575649633cbe2ab80f26733fecda6f5
kcharan <kcharan@knovos.com>
2020-02-27 18:36:12 +0530

    Added test case for ProGurad.main()
    (cherry picked from commit 4392e89386530f0aa8e938f6812f5dec39d54bc7)

---
+++ core/test/proguard/ProGuardTest.java
@@ -0,0 +1,45 @@
+package proguard;
+
+import org.junit.jupiter.api.Test;
+
+class ProGuardTest {
+
+    @Test
+    void main() {
+
+        String targetLocation = "\\";
+        String inputJar = "classes";
+        String outputJar = "proguardClasses";
+        String map_filename = "proguard_map.txt";
+        String seed_filename = "proguard_seed.txt";
+        String method_dir_filename = "method-dictionary.txt";
+        String class_dir_filename = "class-dictionary.txt";
+        String package_dir_filename = "package-dictionary.txt";
+
+        String [] arguments = {
+                "-dontshrink",
+                "-dontoptimize",
+                "-adaptclassstrings",
+                "-keepparameternames",
+                "-keep class com.study.BootWebApplication",
+                "-ignorewarnings",
+                "-injars '" + targetLocation + inputJar + "'",
+                "-outjars '" + targetLocation + outputJar + "'",
+                "-printmapping '" + targetLocation + map_filename + "'",
+                "-printseeds '" + targetLocation + seed_filename + "'",
+
+                "-dontresetmembernaming",
+                "-dontresetclassnaming",
+                "-dontresetpackagenaming",
+
+                "-obfuscationdictionary '" + targetLocation + method_dir_filename + "'",
+                "-classobfuscationdictionary '" + targetLocation + class_dir_filename + "'",
+                "-packageobfuscationdictionary '" + targetLocation + package_dir_filename + "'",
+
+                "-dontwarn java.lang.**",
+                "-dontwarn java.util.**"
+        };
+
+        ProGuard.main(arguments);
+    }
+}
