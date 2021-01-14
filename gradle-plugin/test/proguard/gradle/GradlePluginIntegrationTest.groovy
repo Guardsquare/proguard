737bf24843cf505941c56d1622334e9ff7ac7fb5
Nelson Osacky <nelson@osacky.com>
2021-01-14 17:47:27 +0100

    Add integration test for Gradle Plugin and Spring Boot.

---
+++ gradle-plugin/test/proguard/gradle/GradlePluginIntegrationTest.groovy
@@ -0,0 +1,28 @@
+package proguard.gradle
+
+import org.apache.commons.io.FileUtils
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class GradlePluginIntegrationTest extends Specification {
+    @Rule TemporaryFolder temporaryFolder
+
+    @Unroll
+    def "gradle plugin can be applied to sample"() {
+        def projectRoot = temporaryFolder.newFolder()
+        def fixture = new File(getClass().classLoader.getResource('spring-boot').path)
+        FileUtils.copyDirectory(fixture, projectRoot)
+
+        when:
+        def result = GradleRunner.create()
+        .forwardOutput().withArguments('proguard')
+        .withProjectDir(projectRoot)
+        .build()
+
+        then:
+        result.output =~ "SUCCESSFUL"
+    }
+}
