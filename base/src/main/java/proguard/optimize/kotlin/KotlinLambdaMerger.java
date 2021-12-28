b45bb362ab13ff0094bfdfae706a8973be95c588
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2021-12-28 18:58:47 +0100

    KotlinLambdaMerger: find lambda & function0 classes + find subclasses

--- base/src/main/java/proguard/optimize/kotlin/KotlinLambdaMerger.java
+++ base/src/main/java/proguard/optimize/kotlin/KotlinLambdaMerger.java
@@ -13,6 +13,9 @@ import java.io.IOException;
 
 public class KotlinLambdaMerger {
 
+    private final String KOTLIN_LAMBDA_CLASS = "kotlin/jvm/internal/Lambda";
+    private final String KOTLIN_FUNCTION0_INTERFACE = "kotlin/jvm/functions/Function0";
+
     private static final Logger logger = LogManager.getLogger(KotlinLambdaMerger.class);
     private final Configuration configuration;
 
@@ -25,6 +28,45 @@ public class KotlinLambdaMerger {
                              ClassPool        libraryClassPool,
                              ResourceFilePool resourceFilePool) throws IOException
     {
+        // A class pool where the applicable lambda's will be stored
+        ClassPool lambdaClassPool = new ClassPool();
+
+        // get the Lambda class and the Function0 interface
+        Clazz kotlinLambdaClass = getKotlinLambdaClass(programClassPool, libraryClassPool);
+        Clazz kotlinFunction0Interface = getKotlinFunction0Interface(programClassPool, libraryClassPool);
+        if (kotlinLambdaClass == null) {
+            logger.warn("The Kotlin class '{}' is not found, but it is needed to perform lambda merging.", KOTLIN_LAMBDA_CLASS);
+        }
+        if (kotlinFunction0Interface == null) {
+            logger.warn("The Kotlin class '{}' is not found, but it is needed to perform lambda merging.", KOTLIN_FUNCTION0_INTERFACE);
+        }
+
+        if (kotlinLambdaClass != null && kotlinFunction0Interface != null) {
+            // find all lambda classes of arity 0
+            programClassPool.classesAccept(new ImplementedClassFilter(kotlinFunction0Interface, false,
+                                           new ImplementedClassFilter(kotlinLambdaClass, false,
+                                           new ClassPoolFiller(lambdaClassPool), null), null)
+            );
+            // filter for lambda's without constructor arguments -> put them in a classpool
+            // TODO: filter on classes that have a constructor WITHOUT arguments!
+        }
         return programClassPool;
+    }
+
+    private Clazz getKotlinLambdaClass(ClassPool programClassPool, ClassPool libraryClassPool)
+    {
+        Clazz kotlinLambdaClass = programClassPool.getClass(KOTLIN_LAMBDA_CLASS);
+        if (kotlinLambdaClass == null) {
+            kotlinLambdaClass = libraryClassPool.getClass(KOTLIN_LAMBDA_CLASS);
+        }
+        return kotlinLambdaClass;
+    }
+
+    private Clazz getKotlinFunction0Interface(ClassPool programClassPool, ClassPool libraryClassPool)
+    {
+        Clazz kotlinFunction0Interface = programClassPool.getClass(KOTLIN_FUNCTION0_INTERFACE);
+        if (kotlinFunction0Interface == null) {
+            kotlinFunction0Interface = libraryClassPool.getClass(KOTLIN_FUNCTION0_INTERFACE);
+        }
     }
 }
