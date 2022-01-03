0cd65e81c83f4ca60424cac462457eb88a6e6070
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-01-03 14:31:52 +0100

    KotlinLambdaMergerTest: assert all referenced classes are in class pool

--- base/src/test/kotlin/proguard/optimize/kotlin/KotlinLambdaMergerTest.kt
+++ base/src/test/kotlin/proguard/optimize/kotlin/KotlinLambdaMergerTest.kt
@@ -21,13 +21,17 @@
 
 package proguard.optimize.kotlin
 
-import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.ints.shouldBeGreaterThanOrEqual
 import io.kotest.matchers.shouldBe
+import io.mockk.*
+import proguard.Configuration
 import proguard.classfile.ClassConstants
 import proguard.classfile.ClassPool
+import proguard.classfile.constant.ClassConstant
+import proguard.classfile.constant.visitor.ConstantVisitor
 import proguard.classfile.visitor.ClassMethodFilter
 import proguard.classfile.visitor.ClassPoolFiller
 import proguard.classfile.visitor.ImplementedClassFilter
@@ -37,16 +41,23 @@ import testutils.KotlinSource
 
 class KotlinLambdaMergerTest : FreeSpec({
 
-    // InstancePerTest so the names are reset before every test
-    isolationMode = IsolationMode.InstancePerTest
-
     val (programClassPool, libraryClassPool) = ClassPoolBuilder.fromSource(
         KotlinSource(
             "Test.kt",
             """
             package app.package1
             fun main(index: Int) {
-                val lambda1 = { println("Lambda1") }
+                val lambda1 = { 
+                    println("Lambda1")
+                    val lambda1a = {
+                        println("Lambda1a")
+                        val lambda1a1 = {
+                            println("Lambda1a1")
+                        }
+                        lambda1a1()
+                    }
+                    lambda1a()
+                }
                 val lambda2 = { println("Lambda2") }
                 val lambda3 = { println("Lambda3") }
                 when (index) {
@@ -76,8 +87,8 @@ class KotlinLambdaMergerTest : FreeSpec({
 
     // A class pool where the applicable lambda's will be stored
     val lambdaClassPool = ClassPool()
-    val kotlinLambdaClass = libraryClassPool.getClass(ClassConstants.NAME_KOTLIN_LAMBDA)
-    val kotlinFunction0Interface = libraryClassPool.getClass(ClassConstants.NAME_KOTLIN_FUNCTION0)
+    val kotlinLambdaClass = libraryClassPool.getClass(KotlinLambdaMerger.NAME_KOTLIN_LAMBDA)
+    val kotlinFunction0Interface = libraryClassPool.getClass(KotlinLambdaMerger.NAME_KOTLIN_FUNCTION0)
     programClassPool.classesAccept(ImplementedClassFilter(
                                    kotlinFunction0Interface, false,
                                    ImplementedClassFilter(
@@ -86,18 +97,18 @@ class KotlinLambdaMergerTest : FreeSpec({
                                    ClassConstants.METHOD_NAME_INIT, ClassConstants.METHOD_TYPE_INIT,
                                    ClassPoolFiller(lambdaClassPool), null), null), null))
 
-    "Given a Kotlin Lambda Package Grouper" - {
+    "Given a Kotlin Lambda Package Grouper" {
         val grouper = PackageGrouper()
-        "When the grouper is applied to lambda's of different packages" - {
+        "When the grouper is applied to lambda's of different packages" {
             programClassPool.classesAccept(grouper)
             "Then the grouper has found 2 packages" {
                 grouper.size() shouldBe 2
             }
-            "Then the grouper has found the packages app/package1 and app/package2" - {
+            "Then the grouper has found the packages app/package1 and app/package2" {
                 grouper.containsPackage("app/package1") shouldBe true
                 grouper.containsPackage("app/package2") shouldBe true
             }
-            "Then the grouper does not contain other packages" - {
+            "Then the grouper does not contain other packages" {
                 grouper.packageNames().forEach {
                     it shouldBeIn arrayListOf("app/package2", "app/package1")
                 }
@@ -106,12 +117,49 @@ class KotlinLambdaMergerTest : FreeSpec({
     }
 
     "Given a Kotlin Lambda Merger and entry name mapper" - {
-        val merger = KotlinLambdaMerger(null)
+        val merger = KotlinLambdaMerger(Configuration())
         val nameMapper = ExtraDataEntryNameMap()
         "When the merger is applied to the class pools" - {
             val newProgramClassPool = merger.execute(programClassPool, libraryClassPool, null, nameMapper)
+            val newFullClassPool = newProgramClassPool.classes() union libraryClassPool.classes()
             "Then the resulting program class pool contains less classes" {
                 programClassPool.size() shouldBeGreaterThanOrEqual newProgramClassPool.size()
+            }
+
+            "Then the program classes should only refer to classes that are longer in the class pool" {
+                val visitor = mockk<ConstantVisitor>()
+                val slot = slot<ClassConstant>()
+                every {
+                    visitor.visitAnyConstant(any(), any())
+                    visitor.visitDoubleConstant(any(), any())
+                    visitor.visitDynamicConstant(any(), any())
+                    visitor.visitAnyRefConstant(any(), any())
+                    visitor.visitAnyMethodrefConstant(any(), any())
+                    visitor.visitFieldrefConstant(any(), any())
+                    visitor.visitFloatConstant(any(), any())
+                    visitor.visitIntegerConstant(any(), any())
+                    visitor.visitInterfaceMethodrefConstant(any(), any())
+                    visitor.visitInvokeDynamicConstant(any(), any())
+                    visitor.visitLongConstant(any(), any())
+                    visitor.visitMethodHandleConstant(any(), any())
+                    visitor.visitMethodrefConstant(any(), any())
+                    visitor.visitMethodTypeConstant(any(), any())
+                    visitor.visitModuleConstant(any(), any())
+                    visitor.visitNameAndTypeConstant(any(), any())
+                    visitor.visitPackageConstant(any(), any())
+                    visitor.visitPrimitiveArrayConstant(any(), any())
+                    visitor.visitStringConstant(any(), any())
+                    visitor.visitUtf8Constant(any(), any())
+                } just Runs
+                every {
+                    visitor.visitClassConstant(any(), capture(slot))
+                } answers {
+                    println("Visit class constant referring to ${slot.captured.referencedClass}")
+                    newFullClassPool shouldContain slot.captured.referencedClass
+                }
+                newProgramClassPool.classes().forEach {
+                    it.constantPoolEntriesAccept(visitor)
+                }
             }
         }
     }
