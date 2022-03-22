e91d24a9e894342f74506acbb2127d3799ffc121
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-03-22 22:44:04 +0100

    KotlinLambdaGroupInitUpdater
    A member visitor that adds instructions to a given init method to call the super constructor with the given arity as argument and store the class id in a field

---
+++ base/src/main/java/proguard/optimize/kotlin/KotlinLambdaGroupInitUpdater.java
@@ -0,0 +1,107 @@
+package proguard.optimize.kotlin;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import proguard.classfile.*;
+import proguard.classfile.attribute.*;
+import proguard.classfile.attribute.visitor.AttributeVisitor;
+import proguard.classfile.editor.*;
+import proguard.classfile.instruction.Instruction;
+import proguard.classfile.util.BranchTargetFinder;
+import proguard.classfile.util.ClassUtil;
+import proguard.classfile.util.InstructionSequenceMatcher;
+import proguard.classfile.visitor.MemberVisitor;
+
+public class KotlinLambdaGroupInitUpdater implements AttributeVisitor, MemberVisitor {
+
+    private final ClassPool programClassPool;
+    private final ClassPool libraryClassPool;
+    private static final Logger logger = LogManager.getLogger(KotlinLambdaGroupInitUpdater.class);
+
+    public KotlinLambdaGroupInitUpdater(ClassPool programClassPool,
+                                        ClassPool libraryClassPool)
+    {
+        this.programClassPool      = programClassPool;
+        this.libraryClassPool      = libraryClassPool;
+    }
+
+    @Override
+    public void visitProgramMethod(ProgramClass programClass, ProgramMethod programMethod)
+    {
+        logger.trace("Updating method " + programMethod.getName(programClass) + programMethod.getDescriptor(programClass) + " in class " + programClass);
+        programMethod.attributesAccept(programClass, this);
+    }
+
+    @Override
+    public void visitCodeAttribute(Clazz clazz, Method method, CodeAttribute codeAttribute)
+    {
+        // This attribute visitor should only be used for program classes.
+        try
+        {
+            visitCodeAttribute((ProgramClass) clazz, (ProgramMethod) method, codeAttribute);
+        }
+        catch (ClassCastException exception)
+        {
+            logger.error("{} is incorrectly used to visit non-program class / method {} / {}", this.getClass().getName(), clazz, method);
+        }
+    }
+
+    public void visitCodeAttribute(ProgramClass programClass, ProgramMethod programMethod, CodeAttribute codeAttribute)
+    {
+        CodeAttributeEditor codeAttributeEditor = new CodeAttributeEditor();
+        BranchTargetFinder branchTargetFinder = new BranchTargetFinder();
+
+        // The arity argument is the last of all.
+        // Note that the arguments start counting from 1, as the class itself is at index 0.
+        int arityIndex = ClassUtil.internalMethodParameterCount(programMethod.getDescriptor(programClass));
+
+        InstructionSequencesReplacer replacer = createInstructionSequenceReplacer(branchTargetFinder, codeAttributeEditor, arityIndex, programClass);
+
+        codeAttribute.accept(programClass,
+                             programMethod,
+                             new PeepholeEditor(branchTargetFinder,
+                                                codeAttributeEditor,
+                                                replacer));
+    }
+
+    private InstructionSequencesReplacer createInstructionSequenceReplacer(BranchTargetFinder branchTargetFinder,
+                                                                           CodeAttributeEditor codeAttributeEditor,
+                                                                           int arityIndex,
+                                                                           ProgramClass lambdaGroup)
+    {
+        InstructionSequenceBuilder builder = new InstructionSequenceBuilder(programClassPool, libraryClassPool);
+        Instruction[][][] replacementPatterns = createReplacementPatternsForInit(builder, arityIndex, lambdaGroup);
+        return new InstructionSequencesReplacer(builder.constants(),
+                replacementPatterns,
+                branchTargetFinder,
+                codeAttributeEditor);
+    }
+
+    private Instruction[][][] createReplacementPatternsForInit(InstructionSequenceBuilder builder,
+                                                               int arityIndex,
+                                                               ProgramClass lambdaGroup)
+    {
+        // TODO: store classId in field
+        final int X = InstructionSequenceMatcher.X;
+        return new Instruction[][][]
+                {
+                        {
+                                builder.aload_0()
+                                       .iconst(X)
+                                       .invokespecial(KotlinLambdaMerger.NAME_KOTLIN_LAMBDA, ClassConstants.METHOD_NAME_INIT, "(I)V")
+                                       .__(),
+                                builder.aload_0()
+                                       .iload(arityIndex - 1)
+                                       .putfield(lambdaGroup, // store the id in a field
+                                                 lambdaGroup
+                                                 .findField(KotlinLambdaGroupBuilder.FIELD_NAME_ID,
+                                                            KotlinLambdaGroupBuilder.FIELD_TYPE_ID))
+                                       .aload_0()
+                                       .iload(arityIndex)
+                                       .invokespecial(KotlinLambdaMerger.NAME_KOTLIN_LAMBDA, ClassConstants.METHOD_NAME_INIT, "(I)V")
+                                       .__()
+                        }
+                };
+    }
+}
