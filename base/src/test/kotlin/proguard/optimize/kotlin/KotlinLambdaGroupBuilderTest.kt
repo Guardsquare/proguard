784e884422f111158bbeb4ca737e2a6a3d60a1b2
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-06-07 09:53:18 +0200

    Tests for the KotlinLambdaGroupBuilder

--- base/src/test/kotlin/proguard/optimize/kotlin/KotlinLambdaGroupBuilderTest.kt
+++ base/src/test/kotlin/proguard/optimize/kotlin/KotlinLambdaGroupBuilderTest.kt
@@ -1,15 +1,31 @@
 package proguard.optimize.kotlin
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.ints.shouldBeGreaterThanOrEqual
+import io.kotest.matchers.shouldBe
+import proguard.Configuration
+import proguard.classfile.AccessConstants
+import proguard.classfile.ProgramClass
+import proguard.classfile.attribute.visitor.AllAttributeVisitor
+import proguard.classfile.editor.InstructionSequenceBuilder
+import proguard.classfile.instruction.visitor.AllInstructionVisitor
+import proguard.classfile.util.InstructionSequenceMatcher
+import proguard.classfile.visitor.AllMemberVisitor
+import proguard.classfile.visitor.MemberAccessFilter
+import proguard.classfile.visitor.MemberNameFilter
+import proguard.io.ExtraDataEntryNameMap
+import proguard.optimize.info.ProgramClassOptimizationInfo
+import proguard.optimize.info.ProgramClassOptimizationInfoSetter
 import testutils.ClassPoolBuilder
+import testutils.InstructionSequenceCollector
 import testutils.KotlinSource
+import testutils.MatchDetector
 
 class KotlinLambdaGroupBuilderTest : FreeSpec({
     val (programClassPool, libraryClassPool) = ClassPoolBuilder.fromSource(
         KotlinSource(
-            "Test2.kt",
+            "Test.kt",
             """
-            package app.package2
             fun main() {
                 val lambda1 = { println("Lambda1") }
                 val lambda2 = { println("Lambda2") }
@@ -22,12 +38,74 @@ class KotlinLambdaGroupBuilderTest : FreeSpec({
         )
     )
 
-    "Given a lambda group builder" - {
-        "When the builder is adds a lambda class to the lambda group under construction" - {
-
+    "Given a lambda group builder and a lambda class" - {
+        val lambdaGroupName = "LambdaGroup"
+        val configuration = Configuration()
+        val entryMapper = ExtraDataEntryNameMap()
+        val notMergedLambdaVisitor = null
+        val builder = KotlinLambdaGroupBuilder(
+            lambdaGroupName,
+            configuration,
+            programClassPool,
+            libraryClassPool,
+            entryMapper,
+            notMergedLambdaVisitor
+        )
+        val lambdaClass = programClassPool.getClass("TestKt\$main\$lambda1\$1") as ProgramClass
+        lambdaClass.accept(ProgramClassOptimizationInfoSetter())
+        val optimizationInfo = ProgramClassOptimizationInfo.getProgramClassOptimizationInfo(lambdaClass)
+        val originalInvokeMethodCodeBuilder = InstructionSequenceBuilder(programClassPool, libraryClassPool)
+        lambdaClass.accept(
+            AllMemberVisitor(
+                MemberNameFilter(
+                    "invoke",
+                    MemberAccessFilter(
+                        0, AccessConstants.SYNTHETIC,
+                        AllAttributeVisitor(
+                            AllInstructionVisitor(
+                                InstructionSequenceCollector(
+                                    originalInvokeMethodCodeBuilder
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+        val originalInstructionsMatcher = InstructionSequenceMatcher(
+            originalInvokeMethodCodeBuilder.constants(),
+            originalInvokeMethodCodeBuilder.instructions()
+        )
+        val originalInstructionsMatchDetector = MatchDetector(originalInstructionsMatcher)
+        "When the builder adds a lambda class to the lambda group under construction" - {
+            builder.visitProgramClass(lambdaClass)
+            val lambdaGroup = builder.build()
             "Then the lambda class implementation has been added to the lambda group" {
+                lambdaGroup.accept(
+                    AllMemberVisitor(
+                        AllAttributeVisitor(
+                            AllInstructionVisitor(
+                                originalInstructionsMatchDetector
+                            )
+                        )
+                    )
+                )
+                originalInstructionsMatchDetector.matchIsFound shouldBe true
             }
 
+            "Then the optimization info lambda group of the lambda class refers to the lambda group" {
+                optimizationInfo.lambdaGroup shouldBe lambdaGroup
+            }
+
+            "Then the optimization info target class of the lambda class refers to the lambda group" {
+                optimizationInfo.targetClass shouldBe lambdaGroup
+            }
+
+            "Then the optimization info class id of the lambda class is 0 or greater" {
+                optimizationInfo.lambdaGroupClassId shouldBeGreaterThanOrEqual 0
+            }
+
+            /*
             "Then the lambda group has the same output on invocation as the original lambda class" {
                 /*val stdOutput = System.out
                 val capturedOutputBefore = ByteArrayOutputStream()
@@ -52,6 +130,7 @@ class KotlinLambdaGroupBuilderTest : FreeSpec({
                 println(capturedOutputBefore.toByteArray())
             */
             }
+             */
         }
     }
 })
